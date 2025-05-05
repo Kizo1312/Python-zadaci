fc7906132ce3b6d598368dcdaddab7cf88214860
Kizo1312 <toma.ilcic@gmail.com>
2025-05-05 11:32:00 +0200

    asdfgm

--- predavanje.py
+++ predavanje.py
@@ -3,10 +3,10 @@ x = int(input())
 klime = []
 upaljeni = 0
 ugaseni= 0
-def create ():
+def create (i):
     klima = {
     "on": False,
-    "index": 0,
+    "index": i,
     "count":0
 }
     return klima
@@ -14,18 +14,17 @@ def create ():
 
 
 for i in range(n):
-    klime.append(create())
+    klime.append(create(i +1))
     
    
-for i in range(len(klime) ):
-    klime[i]["index"] =i+1
+
 
 print(klime)
 print(len(klime))
 
 for i in range (x):
     si = int(input())
-    if si == klime[i]["index"]:
+    if si == klime[si]["index"]:
         klime[i]["count"] +=1
         if klime[i]["count"] %2 == 0:
             klime[i]["on"]= False
--- prosjek.py
+++ prosjek.py
@@ -1,11 +1,20 @@
 ocjene = list(map(int, input().split(" ")))
 zbroj = 0
+count =0
 for i in range (len(ocjene)):
     zbroj+=ocjene[i]
 
 prosjek = round(zbroj / len(ocjene), 2)
 
-if prosjek >= 4.5:
+for i in range(len(ocjene)):
+    if ocjene[i] == 1:
+        count+=1
+print(count)
+
+
+if count >0:
+    print("nedovoljan")
+elif prosjek >= 4.5:
     print(prosjek, "odlican")
 elif prosjek >= 3.5 and prosjek <4.5:
     print(prosjek, "vrlo dobar")
