b297bd9c7401b4be934788a3a66ff73edfa42bf0
Kizo1312 <toma.ilcic@gmail.com>
2025-05-05 10:29:35 +0200

    ajde

--- pretvorba.py
+++ pretvorba.py
@@ -1,8 +1,10 @@
+t = int(input())
+u = input()
 def pretvorba(temp, unit):
     if unit == "C":
         temp = temp *(9/5) + 32
     else:
         temp = (temp - 32) * 5/9
     return temp
-print(pretvorba(25, "F"))
+print(pretvorba(t, u))
 
