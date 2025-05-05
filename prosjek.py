5ed5836455b73553454caea2205a04ca6356b85d
Kizo1312 <toma.ilcic@gmail.com>
2025-05-05 11:13:36 +0200

    prosjek

---
+++ prosjek.py
@@ -0,0 +1,17 @@
+ocjene = list(map(int, input().split(" ")))
+zbroj = 0
+for i in range (len(ocjene)):
+    zbroj+=ocjene[i]
+
+prosjek = round(zbroj / len(ocjene), 2)
+
+if prosjek >= 4.5:
+    print(prosjek, "odlican")
+elif prosjek >= 3.5 and prosjek <4.5:
+    print(prosjek, "vrlo dobar")
+elif prosjek >= 2.5 and prosjek <3.5:
+    print(prosjek, " dobar")
+elif prosjek >= 1.5 and prosjek <2.5:
+    print(prosjek, "dovoljan")
+else:
+    print(prosjek, "nedovoljan")
