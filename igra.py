155c4d15dacec40fe92a37a92dc036dd30e9dcf7
Toma Ilcic <toma.ilcic@gmail.com>
2025-05-12 19:16:00 +0200

    layout

--- igra.py
+++ igra.py
@@ -2,39 +2,12 @@ from tkinter import *
 istina = True
 
 
-def dodaj1():
-    
-    labela.config(text=f"{labela.cget("text")} 1")
-def dodaj2():
-    labela.config(text=f"{labela.cget("text")} 2")
-def dodaj3():
-    labela.config(text=f"{labela.cget("text")} 3")
-def dodaj4():
-    labela.config(text=f"{labela.cget("text")} 4")
-def dodaj5():
-    labela.config(text=f"{labela.cget("text")} 5")
-def dodaj6():
-    labela.config(text=f"{labela.cget("text")} 6")
-def dodaj7():
-    labela.config(text=f"{labela.cget("text")} 7")
-def dodaj8():
-    labela.config(text=f"{labela.cget("text")} 8")
-def dodaj9():
-    labela.config(text=f"{labela.cget("text")} 9")
-def dodaj_plus():
-    labela.config(text =f"{labela.cget("text")} +")
+
 def dodaj_jednako():
-    labela.config(text=eval(labela.cget("text")))
+    labela.config(text=eval(labela.cget('text')))
 
-def dodaj_minus():
-    labela.config(text =f"{labela.cget("text")} -")
-
-def dodaj_puta():
-    labela.config(text =f"{labela.cget("text")} *")
-
-def dodaj_divide():
-    labela.config(text =f"{labela.cget("text")} /")
-
+def dodaj_znak(znak):
+    labela.config(text=f"{labela.cget('text')}{znak}")
 
 
 
@@ -43,49 +16,51 @@ root.title("demo")
 
 
 
-unos = Entry(root, width=30)
-unos.grid(row=0, column=0)
-plus = Button(root,text="+", command=dodaj_plus )
+
+plus = Button(root,text="+", command= lambda:dodaj_znak("+") )
 plus.grid(row=5, column=0)
 
-jedinica = Button(root, text="1", command=dodaj1)
-jedinica.grid(row=0, column=3)
+jedinica = Button(root, text="1", command=lambda:dodaj_znak("1"))
+jedinica.grid(row=3, column=0)
 
-dvojka = Button(root, text="2", command=dodaj2)
-dvojka.grid(row=1, column=3)
 
-trojka = Button(root, text="3", command=dodaj3)
-trojka.grid(row=2, column=3)
+dvojka = Button(root, text="2", command=lambda:dodaj_znak("2"))
+dvojka.grid(row=3, column=1)
 
-cetvorka = Button(root, text="4", command=dodaj4)
-cetvorka.grid(row=3, column=3)
+trojka = Button(root, text="3", command=lambda:dodaj_znak("3"))
+trojka.grid(row=3, column=2)
 
-petica = Button(root, text="5", command=dodaj5)
-petica.grid(row=4, column=3)
+cetvorka = Button(root, text="4", command=lambda:dodaj_znak("4"))
+cetvorka.grid(row=2, column=0)
 
-sestica = Button(root, text="6", command=dodaj6)
-sestica.grid(row=5, column=3)
+petica = Button(root, text="5", command=lambda:dodaj_znak("5"))
+petica.grid(row=2, column=1)
 
-sedmica = Button(root, text="7", command=dodaj7)
-sedmica.grid(row=6, column=3)
+sestica = Button(root, text="6", command=lambda:dodaj_znak("6"))
+sestica.grid(row=2, column=2)
 
-osmica = Button(root, text="8", command=dodaj8)
-osmica.grid(row=7, column=3)
+sedmica = Button(root, text="7", command=lambda:dodaj_znak("7"))
+sedmica.grid(row=1, column=0)
 
-devetka = Button(root, text="9", command=dodaj9)
-devetka.grid(row=8, column=3)
+osmica = Button(root, text="8", command=lambda:dodaj_znak("8"))
+osmica.grid(row=1, column=1)
+
+devetka = Button(root, text="9", command=lambda:dodaj_znak("9"))
+devetka.grid(row=1, column=2)
 
 jednako = Button(root, text="=", command=dodaj_jednako)
-jednako.grid(row=9, column=3)
+jednako.grid(row=4, column=1)
 
-minus = Button(root, text="-", command=dodaj_minus)
-minus.grid(row=6, column=0)
+minus = Button(root, text="-", command=lambda:dodaj_znak("-"))
+minus.grid(row=3, column=3)
 
-puta = Button(root, text="*", command=dodaj_puta)
-puta.grid(row=7, column=0)
+puta = Button(root, text="*", command=lambda:dodaj_znak("*"))
+puta.grid(row=2, column=3)
 
-podijeljeno = Button(root, text="/", command=dodaj_divide)
-podijeljeno.grid(row=8, column=0)
+podijeljeno = Button(root, text="/", command=lambda:dodaj_znak("/"))
+podijeljeno.grid(row=1, column=3)
+
+
 
 
 
@@ -93,11 +68,13 @@ podijeljeno.grid(row=8, column=0)
 
 
 labela = Label(root, text="")
-labela.grid(row=2,column=0)
+labela.grid(row=0, column=0, columnspan=4)
 
-Button(root, text="quit", command=root.destroy).grid(column=3, row=5)
+nula = Button(root, text="0", command=lambda:dodaj_znak("0"))
+nula.grid(row=4, column=0)
 
 
-
+clear = Button(root, text="C", command=lambda: labela.config(text=""))
+clear.grid(row=4, column=2)
 
 root.mainloop()
