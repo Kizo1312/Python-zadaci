fda7a292ee20deaec05f3db3440a3fe8c946172c
Kizo1312 <toma.ilcic@gmail.com>
2025-05-13 09:47:22 +0200

    jhgf

--- rekurz_string.py
+++ rekurz_string.py
@@ -8,3 +8,15 @@ def obrni(rijec):
     else:
         return rijec[-1] + obrni(rijec[:-1]) 
 print(obrni("toma"))
+
+
+def palindrom(rijec):
+    if len(rijec) ==0 or len(rijec) == 1:
+        return True
+    elif rijec[0]!=rijec[-1]:
+        return False
+        
+    else: return palindrom(rijec[1:-1])
+
+    
+print(palindrom("radar"))
